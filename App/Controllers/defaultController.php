7d108f8f3b05315d578fd3f6e6b78ef31263e8a4
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-10-09 15:42:37 +0200

    render view

--- App/Controllers/defaultController.php
+++ App/Controllers/defaultController.php
@@ -3,13 +3,21 @@
 class defaultController {
     public function index() {
         //cridar a la vista corresponent
-        // $this -> render("prova");
-        echo"estic al controller main";
+        
+        // echo"estic al controller default";
+        $params['title']="Home";
+        $this->render('mp/index',$params,'main');
     }
 
 protected function render($path,$params=[],$layout=""){
     //rennderitzar la vista
-    echo "estic a punt per renderitzar la vista del Main Controller";
+    // echo "estic a punt per renderitzar la vista del Main Controller";
+    ob_start();
+    require_once(__DIR__ . "/../Views/" . $path . ".view.php");
+    $params['content']=ob_get_clean();
+    require_once(__DIR__ . "/../Views/layouts/" . $layout . ".layout.php");
+    
+
 }
 
 }
