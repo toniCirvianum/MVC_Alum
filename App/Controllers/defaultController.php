d6c3840b47d802256680abf44ab1bdcf650d30df
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-10-10 19:23:29 +0200

    autoload done

--- App/Controllers/defaultController.php
+++ App/Controllers/defaultController.php
@@ -1,12 +1,12 @@
 <?php
 
 class defaultController extends Controller {
-    public function index() {
+    public function index($values=null) {
         //cridar a la vista corresponent
-        
+        print_r($values);
         // echo"estic al controller default";
-        $params['title']="Home";
-        $this->render('home/index',$params,'main');
+        // $params['title']="Home";
+        // $this->render('home/index',$params,'main');
     }
 
 
--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
 
-require_once('App/Router.php');
-require_once('App/Core/Controller.php');
-require_once('App/Model/Orm.php');
-require_once('App/Model/Mp.php');
-
-use App\Router;
-
 if (!isset($_SESSION)) {
     session_start();
 }
+// require_once('App/Router.php');
+// require_once('App/Core/Controller.php');
+// require_once('App/Model/Orm.php');
+// require_once('App/Model/Mp.php');
+
+use App\Router;
+
+
+spl_autoload_register(function($classe) {
+    str_replace('\\','/',$classe);
+    $classe=$classe.".php";
+    require_once($classe);
+});
 
 
 
 $myRouter = new Router();
-$myRouter->run();
+// $myRouter->run();
+
+
